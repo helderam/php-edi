351125edc7fe0956bd0ce1e99f084b61c4d5784e
ivan <ivan.silva@vilanova.com.br>
2019-12-09 15:22:35 -0300

    EDI pernod

--- GeraPernod.php
+++ GeraPernod.php
@@ -47,27 +47,44 @@ try {
     // Variavel memória de todas as linhas
     $linhas = '';
 
+    date_default_timezone_set("Brazil/East");
+
+    $data = Date('d-m-Y H:i:s');
+
+    $data = str_replace ([':','-',' '], '' , $data);
+    $segundos = Date('s');
+        
+    $milesimos = round($segundos * 1000); 
     // Instancia objetos
     $venda = new Venda();
 
-
+    
     // Obtem os dados de arquivo XML: dados/proceda.xml
     $xml = simplexml_load_file('dados/Pernod.xml');
+    
     // Acrescenta registro
-    $linhas = $venda->header_venda($xml->identificador, $xml->cnpj_fornecedor);
+    $linhas = $venda->header_venda($xml->identificador, $xml->cnpj_fornecedor)."\n";
+    
 
-    $linhas .= $venda->detalhe_venda($xml->movimento_de_vendas->cnpj_distribuicao,$xml->movimento_de_vendas->identificacao_do_cliente);
-
-/*    file_put_contents('arquivos/ESTOQUE_00000000_20150701.txt', $linhas);
+    $linhas .= $venda->detalhe_venda($xml->movimento_de_vendas->cnpj_distribuicao, $xml->movimento_de_vendas->identificacao_do_cliente, 
+                                     $xml->movimento_de_vendas->data_transacao, $xml->movimento_de_vendas->numero_documento, 
+                                     $xml->movimento_de_vendas->codigo_do_produto, $xml->movimento_de_vendas->quantidade, 
+                                     $xml->movimento_de_vendas->preco_de_venda,$xml->movimento_de_vendas->codigo_vendedor,
+                                     $xml->movimento_de_vendas->campo_reservado, $xml->movimento_de_vendas->tipo_documento,
+                                     $xml->movimento_de_vendas->cep, $xml->movimento_de_vendas->codigo_lote, $xml->movimento_de_vendas->validade_lote,
+                                     $xml->movimento_de_vendas->dia_validade_lote,$xml->movimento_de_vendas->pedido_sugerido,
+                                     $xml->movimento_de_vendas->preco_de_venda_dolar,$xml->movimento_de_vendas->tipo_de_unidade);
+    
+   file_put_contents("arquivos/VENDA{$data}{$milesimos}.txt", $linhas);
     echo $linhas."\n";
-    echo "\n\n";*/
+     echo "\n\n";
 
     
-   /* $linhas = $sellout->header_sellout($xml->cod_sap, $xml->sellout->data)."\n";
+   /* $linhas = $sellout->header_estoque($xml->cod_sap, $xml->sellout->data)."\n";
     
     $linhas .= $sellout->detalhe_sellout($xml->sellout->cod_sap_dist, $xml->sellout->cod_sap_ent, $xml->sellout->cod_cliente, $xml->sellout->nome_cliente, $xml->sellout->cnpj_cliente_indireto, $xml->sellout->cpf_cliente_indireto, $xml->sellout->endereco, $xml->sellout->cep, $xml->sellout->estado, $xml->sellout->bairro, $xml->sellout->municipio, $xml->sellout->telefone, $xml->sellout->pais, $xml->sellout->contato, $xml->sellout->tipo_negocio, $xml->sellout->zona_estabelecida_distribuidor, $xml->sellout->representante_distribuidor, $xml->sellout->nome_representante, $xml->sellout->cod_produto_atacado, $xml->sellout->cod_ean, $xml->sellout->cod_sap_produto, $xml->sellout->venda_qntd, $xml->sellout->valor_venda, $xml->sellout->preco_unitario, $xml->sellout->tipo_moeda, $xml->sellout->data_faturamento, $xml->sellout->data_entrega, $xml->sellout->tipo_documento, $xml->sellout->cod_documento, $xml->sellout->unidade_medida_qntd, $xml->sellout->numerador_conversao_qntd_unidade, $xml->sellout->denominador_conversor_qntd_unidade, $xml->sellout->unidade_medida_preco, $xml->sellout->numerador_conversao_preco_unidade, $xml->sellout->denominador_conversor_preco_unidade, $xml->sellout->data_fim_periodo_reportado, $xml->sellout->data_transmissao_informacao, $xml->sellout->numero_transmissao, $xml->sellout->cpf_representante_vendas_atacado, $xml->sellout->chave_acesso);
-    file_put_contents('arquivos/SELLOUT_CodigoFIA_AAAAMMDD.txt', $linhas);*/
-    echo $linhas."\n";
+    file_put_contents('arquivos/SELLOUT_CodigoFIA_AAAAMMDD.txt', $linhas);
+   // echo $linhas."\n";*/
 
 } catch (Exception $e) {
     echo 'ERRO: ' .$e->getMessage();
--- app/Pernod/Venda.php
+++ app/Pernod/Venda.php
@@ -13,7 +13,7 @@
  * |                                                                       |
  * | Autor......: Ivan <ivan.silva@vilanova.com.br>                        |
  * |                                                                       |
- * | Criação....: 29-44-2019                                               |
+ * | Criação....: 29-12-2019                                               |
  * |                                                                       |
  * | Objetivo...: Formatar registros conforme Layout Pernod Venda          | 
  * |                                                                       |
@@ -50,16 +50,35 @@ class Venda
         return $linha;
     }
 
-    public function detalhe_venda($cnpj_distribuicao, $identificacao_do_cliente)
+    public function detalhe_venda($cnpj_distribuicao, $identificacao_do_cliente, $data_transacao, $numero_documento, 
+                                    $codigo_do_produto, $quantidade, $preco_de_venda,$codigo_vendedor, $campo_reservado,
+                                    $tipo_documento, $cep, $codigo_lote, $validade_lote, $dia_validade_lote, $pedido_sugerido,
+                                    $preco_de_venda_dolar, $tipo_de_unidade)
     {
         $layout = new Layout();
          
         // CAMPO: ORDEM, DESCRIÇÃO, CONTEUDO, TIPO N/A, TAMANHO, OBRIGATORIO, PREENCHIMENTO
         $layout->adiciona(new Campo(1, 'TIPO DO REGISTRO', 'D', ALFA, 1, OBRIGATORIO, PREENCHIMENTO));
-        $layout->adiciona(new Campo(2, 'CNPJ DISTRIBUIÇÃO', $cnpj_distribuicao, NUMERICO, 14, OBRIGATORIO, PREENCHIMENTO));
-        $layout->adiciona(new Campo(3, 'INDENTIFICAÇÃO CLIENTE', $identificacao_do_cliente, ALFA, 18, OBRIGATORIO, PREENCHIMENTO));
-        $layout->adiciona(new Campo(4, 'INDENTIFICAÇÃO CLIENTE', $identificacao_do_cliente, ALFA, 18, OBRIGATORIO, PREENCHIMENTO));
+        $layout->adiciona(new Campo(2, 'INDENTIFICAÇÃO CLIENTE', $identificacao_do_cliente, ALFA, 18, OBRIGATORIO, PREENCHIMENTO));
+        $layout->adiciona(new Campo(3, 'CNPJ DISTRIBUIÇÃO', $cnpj_distribuicao, NUMERICO, 14, OBRIGATORIO, PREENCHIMENTO));
+        $layout->adiciona(new Campo(4, 'DATA TRANSAÇÃO', $data_transacao, ALFA, 8, OBRIGATORIO, PREENCHIMENTO));
+        $layout->adiciona(new Campo(5, 'NUMERO DOCUMENTO', $numero_documento, ALFA, 20, OBRIGATORIO, PREENCHIMENTO));
+        $layout->adiciona(new Campo(6, 'CODIGO DO PRODUTO', $codigo_do_produto, NUMERICO, 14, OBRIGATORIO, PREENCHIMENTO));
+        $layout->adiciona(new Campo(7, 'QUANTIDADE', $quantidade, NUMERICO, 15.4, OBRIGATORIO, PREENCHIMENTO));
+        $layout->adiciona(new Campo(8, 'PREÇO DE VENDA', $preco_de_venda, NUMERICO, 5.2, OBRIGATORIO, PREENCHIMENTO));
+        $layout->adiciona(new Campo(9, 'CODIGO VENDEDOR', $codigo_vendedor, ALFA, 20, OBRIGATORIO, PREENCHIMENTO));
+        $layout->adiciona(new Campo(10,'CAMPO RESERVADO', $campo_reservado, ALFA, 10, OPCIONAL, PREENCHIMENTO));
+        $layout->adiciona(new Campo(11, 'TIPO DOCUMENTO', $tipo_documento, ALFA, 10, OBRIGATORIO, PREENCHIMENTO));
+        $layout->adiciona(new Campo(12, 'CEP', $cep, NUMERICO, 8, OBRIGATORIO, PREENCHIMENTO));
+        $layout->adiciona(new Campo(13, 'CODIGO LOTE', $codigo_lote, ALFA, 13, OPCIONAL, PREENCHIMENTO));
+        $layout->adiciona(new Campo(14, 'VALIDADE LOTE', $validade_lote, NUMERICO, 6, OPCIONAL, PREENCHIMENTO));
+        $layout->adiciona(new Campo(15, 'DIA VALIDADE LOTE', $dia_validade_lote, NUMERICO, 2, OPCIONAL, PREENCHIMENTO));
+        $layout->adiciona(new Campo(16, 'PEDIDO SUGERIDO', $pedido_sugerido, ALFA, 1, OPCIONAL, PREENCHIMENTO));
+        $layout->adiciona(new Campo(17, 'PREÇO DE VENDA DOLAR', $preco_de_venda_dolar, NUMERICO, 5.2, OPCIONAL, PREENCHIMENTO));
+        $layout->adiciona(new Campo(18, 'TIPO DE UNIDADE', $tipo_de_unidade, NUMERICO, 4, OBRIGATORIO, PREENCHIMENTO));
 
+        
+       
         $linha = $layout->gera_linha();
         // Gera linha conforme o layout
         return $linha;
